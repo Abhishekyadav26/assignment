2cff168c4e17169da9af297d66006ac748954699
Abhishekyadav26 <aky732244@gmail.com>
2023-09-03 13:07:21 +0530

    just added the file sum.c

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("enter :");
+    scanf("%d",&a);
+    printf("enter :");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("the sum is :%d",sum);
+    return 0;
+}
